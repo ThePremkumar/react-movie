cf7506086dee1c3102475ab30b172d186e7ccfa0
ThePremkumar <spremkumar2424@gmail.com>
2026-07-14 09:54:28 +0530

    feat: integrate Appwrite to track search trends and display trending movies in the UI

--- src/appwrite.js
+++ src/appwrite.js
@@ -3,9 +3,10 @@ import {Client, Databases, ID, Query} from "appwrite";
 const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
 const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
+const ENDPOINT = import.meta.env.VITE_APPWRITE_ENDPOINT || 'https://cloud.appwrite.io/v1';
 
 const client = new Client()
-    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setEndpoint(ENDPOINT)
     .setProject(PROJECT_ID)
 
 const database = new Databases(client);
@@ -48,5 +49,6 @@ export const getTrendingMovies = async() =>{
         return result.documents;
     }catch (error){
         console.error(error);
+        return [];
     }
 }
